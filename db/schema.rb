8551ceca428b4d688ce727e68725b80df667ae06
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-06-08 18:25:58 +0900

    20_profile_edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_04_000050) do
+ActiveRecord::Schema.define(version: 2023_06_08_080332) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2023_06_04_000050) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
